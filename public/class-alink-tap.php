66b6faf0dfb818629267f9ee65ec965b4de5c467
todoapuestas <todoapuestas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-11 19:09:39 +0000

    releasing version 1.0.1.2
    git-svn-id: https://plugins.svn.wordpress.org/alink-tap/tags/1.0.1.2@1087389 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- public/class-alink-tap.php
+++ public/class-alink-tap.php
@@ -71,8 +71,8 @@ class Alink_Tap {
 		add_action( 'wpmu_new_blog', array( $this, 'activate_new_site' ) );
 
 		// Load public-facing style sheet and JavaScript.
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+//		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+//		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 
         /**
          * Define the default options
